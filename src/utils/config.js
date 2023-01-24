72b55fc673cac7a33d1c64b5f91bdca701cae29a
Sunny Kumar <chemicalsunnyraj@gmail.com>
2023-01-23 22:15:35 -0500

    Add files via upload

---
+++ src/utils/config.js
@@ -0,0 +1,9 @@
+const config = {
+  siteTitle: 'mahakaal',
+  siteUrl: 'https://www.mahakaal.com',
+  siteLogo: '/logo.png',
+  description:
+    'Software engineer and open source creator. This is my digital garden.',
+}
+
+export default config
